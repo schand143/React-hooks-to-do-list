b23f69e55c23902808afb18e7e365e0316fcc4b4
schand143 <shaikhchand724@gmail.com>
2020-02-27 19:35:49 +0530

    Updates

---
+++ precache-manifest.204605bc2e72692c5612cd1fe31cf3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f2a8c9c085a3b1ce8833b15cecc8a5",
+    "url": "/React-hooks-to-do-list/index.html"
+  },
+  {
+    "revision": "49d993c19e1c5d5b8da2",
+    "url": "/React-hooks-to-do-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "349aec744b6c8b67db43",
+    "url": "/React-hooks-to-do-list/static/js/2.f7ec3191.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-hooks-to-do-list/static/js/2.f7ec3191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d993c19e1c5d5b8da2",
+    "url": "/React-hooks-to-do-list/static/js/main.1bcec3a6.chunk.js"
+  },
+  {
+    "revision": "cde37fb4b789780cda42",
+    "url": "/React-hooks-to-do-list/static/js/runtime-main.cdd5bf16.js"
+  }
+]);
